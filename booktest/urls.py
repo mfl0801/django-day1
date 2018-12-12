66274e6374dfa5b202efc447e4653cc0da4acd1a
mfl0801 <mfl0801@126.com>
2018-12-12 15:41:00 +0800

    20181212

---
+++ booktest/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from booktest import views
+urlpatterns = [
+    # 通过url函数设置url路由配置项
+    url(r'^index$', views.index), # 建立/index和视图index之间的关系
+#    url(r'^index2$', views.index2),
+#    url(r'^books$', views.show_books), # 显示图书信息
+#    url(r'^books/(\d+)$', views.detail), # 显示英雄信息
+]
--- booktest/views.py
+++ booktest/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader,RequestContext
 # Create your views here.
+def index(request):
+    temp = loader.get_template('booktest/index.html')
+    context = RequestContext(request, {})
+    res_html = temp.render(context)
+    return HttpResponse(res_html)
